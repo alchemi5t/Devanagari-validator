6fbea419aacedb33fccc926b5363f9434175c71d
alchemi5t <soundcrane@gmail.com>
2018-06-04 22:15:51 +0530

    Update dev_validator.py

--- dev_validator.py
+++ dev_validator.py
@@ -2,95 +2,71 @@ import sublime
 import sublime_plugin
 import re
 
+
+
+consonants=["ॿ","ॾ","ॼ","ॻ","क","ख","ग","घ","ङ","च","छ","ज","झ","ञ","ट","ठ","ड","ढ","ण","त","थ","द","ध","न","प","फ","ब","भ","म","य","र","ऱ","ल","ळ","व","श","ष","स","ह"]
+vowels=["ॷ","ॶ","ॵ","ॴ","ॳ","ॲ","अ","आ","इ","ई","उ","ऊ","ऋ","ऍ","ऎ","ए","ऐ","ऑ","ऒ","ओ","औ","ॠ"]
+matras=["ा","ि","ी","ु","ू","ृ","ॅ","ॆ","े","ै","ॉ","ॊ","ो","ौ","ऻ","ऺ","ॄ","ॆ","ॏ","ॗ","ॖ"]
+vowelMod=["ँ","ं","ः"]
+halant=["्"]
+nukta=["़"]
+Charset='[^'+''.join(consonants+vowels+matras+vowelMod+halant+nukta)+']'
+C='['+''.join(consonants)+']'
+N='['+''.join(nukta)+']'
+H='['+''.join(halant)+']'
+D='['+''.join(vowelMod)+']'
+M='['+''.join(matras)+']'
+V='['+''.join(vowels)+']'
+# reg=r'^((((('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+')))?(('+C+')('+N+')?)('+M+')?('+D+')?|('+V+')('+D+')?))+((('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+')))?$'
+reg = r'''
+(?x)
+(
+    (
+        (({C}{N}?{H})?({C}{N}?{H})?({C}{N}?{H}))?
+        ({C}{N}?){M}?{D}?
+    )
+    |{V}{D}?
+)+
+(
+    ({C}{N}?{H})?
+    ({C}{N}?{H})?
+    ({C}{N}?{H})
+)?
+$
+'''.format(C=C, N=N, H=H, D=D, V=V, M=M)
+
+
+
+
 class isbetter(sublime_plugin.EventListener):
 
-	invalid_words_set = u'|'.join([
-		'\u200b',  
-		'\u200c',  
-		'\u200d',   
-		'\u2060',   
-		'\u2061', 
-		'\u2062', 
-		'\u2063',
-		'\u2064',
-		'\u2065',  
-		'\u2066',  
-		'\u2067',  
-		'\u2068', 
-		'\u2069',
-		'\u206a',   
-		'\u206b',
-		'\u206c',
-		'\u206d', 
-		'\u206e',  
-		'\u206f',  
-		'\ufeff',   
-	])
-
-
-
-
-
-
 	def dothings(self, view):
-
-		consonants=["ॿ","ॾ","ॼ","ॻ","क","ख","ग","घ","ङ","च","छ","ज","झ","ञ","ट","ठ","ड","ढ","ण","त","थ","द","ध","न","प","फ","ब","भ","म","य","र","ऱ","ल","ळ","व","श","ष","स","ह"]
-		vowels=["ॷ","ॶ","ॵ","ॴ","ॳ","ॲ","अ","आ","इ","ई","उ","ऊ","ऋ","ऍ","ऎ","ए","ऐ","ऑ","ऒ","ओ","औ","ॠ"]
-		matras=["ा","ि","ी","ु","ू","ृ","ॅ","ॆ","े","ै","ॉ","ॊ","ो","ौ","ऻ","ऺ","ॄ","ॆ","ॏ","ॗ","ॖ"]
-		vowelMod=["ँ","ं","ः"]
-		halant=["्"]
-		nukta=["़"]
-
-		C='['+''.join(consonants)+']'
-		N='['+''.join(nukta)+']'
-		H='['+''.join(halant)+']'
-		D='['+''.join(vowelMod)+']'
-		M='['+''.join(matras)+']'
-		V='['+''.join(vowels)+']'
-		reg=r'((((('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+')))?(('+C+')('+N+')?)('+M+')?('+D+')?|('+V+')('+D+')?))+((('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+'))?(('+C+')('+N+')?('+H+')))?$'
 		content = view.substr(sublime.Region(0, view.size()))
-		# input_text=ip_file.read()
-		words = re.split("\n| ",content)
-		for idx,word in enumerate(words):
-			if len(word)==0:
-				del words[idx]
-		# print(words)
-		# a=words
-		# for word in words:
-			# a.append(word.split(",")[0])
-		# words = a
+		words = re.split("\n|।| |"+Charset,content)
 		valid_words=[]
-		# i_n=0
-		print(words)
-		for word in words:
-			if(re.match(reg,word)):
-				print(word)
-				valid_words.append(word)
-
-		# self.invalid_words_set=u'|'.join(temp)
+		valid_words = [ word for word in words if re.match(reg, word) and word not in valid_words ]
 		view.erase_regions('invalid')
 		for idx,i in enumerate(valid_words):
 			valid_words[idx]=r''+valid_words[idx]+r''
-		# print("$|".join(valid_words))
-		regexf="|".join(valid_words)
-		regexf+=""
-		print(regexf)
+		regexf=" |".join(valid_words)
 		regions = view.find_all(regexf)
 		if regions:
-			view.add_regions('zero-width', regions, 'invalid')
+			view.add_regions('valid', regions, 'valid')
 
 
 	def on_load_async(self, view):
 		try:
 			self.dothings(view)
-		except e:
-			print(e)
+		except:
+			print("broke")
 	def on_post_save_async(self, view):
+		try:
+			self.dothings(view)
+		except:
+			print("broke")
 
-		self.dothings(view)
 
 
-# print(re.match(reg,'ॿिघ'))
 
 
 
